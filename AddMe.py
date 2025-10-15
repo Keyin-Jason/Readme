ff9f57f22f0f6a5ae3e2b2a7c08921e65aa243c0
Keyin-Jason <jason.legge@keyin.com>
2025-10-15 10:39:15 -0230

    Add files via upload

---
+++ AddMe.py
@@ -0,0 +1,12 @@
+# Program to add two nimbers together
+
+print()
+NUM_ONE = 3
+NUM_TWO = 5
+
+print()
+SumTwo = NUM_ONE + NUM_TWO
+
+print ("The answer is: ", SumTwo)
+print()
+print()
---
+++ Stuff.py
@@ -0,0 +1,11 @@
+# A program designed to do somthing silly.
+
+print()
+name = input("What is your name? ")
+age = input("What is your age? ")
+age = int(age)
+if age >= 18:
+    print("Welcome to adulthood")
+else:
+    print("You still got time, have fun")
+
